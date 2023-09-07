b8ea83073e8d2fc6c6f2c9171f0b87f021c6b69c
evgeni batanov <batanovevgeni@mail.ru>
2023-09-07 14:07:10 +0300

    07.09.2023

--- src/moex/Run.java
+++
@@ -1,9 +0,0 @@
-package moex;
-
-public class Run {
-    public static void main(String[] args) {
-//        "https://www.moex.com/api/contract/OpenOptionService/" "/F/" "/json"
-        System.out.println(MoexGetData.Run( "https://www.moex.com/api/contract/OpenOptionService/",
-                "01.08.2023", "/F/", "ALRS", "/json"));
-    }
-}
