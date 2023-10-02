c15aa32e195f1ac6c34e8192ce52309cc10f01e0
Evgeni Batanov <batanovevgeni@mail.ru>
2023-10-02 18:38:27 +0300

    02.10.2023

--- src/priceCalculator/PriceCalculatorTest.java
+++ src/priceCalculator/PriceCalculatorTest.java
@@ -16,6 +16,56 @@ class PriceCalculatorTest {
         priceCalculator = new PriceCalculator();
     }
 
+    @Test
+    @DisplayName("Test CalculatorPrice incorrect transport")
+    void calculatePriceForIncorrectTransport() {
+        UnsupportedOperationException exception = Assertions.assertThrows(UnsupportedOperationException.class,() -> {
+            priceCalculator.calculatePrice(TransportType.DRONE, 10);
+        },"transport type is not handled correctly");
+    }
+
+    @Test
+    @DisplayName("Test CalculatorPrise track valid value")
+    void calculatePriceForTrackIsValidArgument() {
+        int expected = 50;
+        int actual = priceCalculator.calculatePrice(TransportType.TRUCK, 10);
+        Assertions.assertEquals(expected, actual, "Неверный результат");
+    }
+
+    @Test
+    @DisplayName("Test CalculatorPrise track negative value")
+    void calculatePriceForTrackIsNegativeValue() {
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            priceCalculator.calculatePrice(TransportType.TRUCK, 0);
+        });
+        Assertions.assertEquals("Distance should be more than 0 km", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Test CalculatorPrice car valid value")
+    void calculatePriceForCarIsValidArgument(){
+        int expected = 70;
+        int actual = priceCalculator.calculatePrice(TransportType.CAR, 10);
+        Assertions.assertEquals(expected, actual, "Неверный результат");
+    }
+
+    @Test
+    @DisplayName("Test CalculatorPrise car overriding value")
+    void calculatePriceForCarIsOverridingValue(){
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class,()->{
+            priceCalculator.calculatePrice(TransportType.CAR, 1001);
+        });
+        Assertions.assertEquals("Car can not go for more than 1000 km",exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Test CalculatorPrise car negative value")
+    void calculatePriceForCarIsNegativeValue(){
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class,()->{
+            priceCalculator.calculatePrice(TransportType.CAR, 0);
+        });
+        Assertions.assertEquals("Distance should be more than 0 km", exception.getMessage());
+    }
 
     @Test
     @DisplayName("Test CalculatorPrise bike valid value")
@@ -29,7 +79,7 @@ class PriceCalculatorTest {
     @DisplayName("Test CalculatorPrise bike negative value")
     void calculatePriceForBikeIsNegativeValue() {
         IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            priceCalculator.calculatePrice(TransportType.BIKE, -1);
+            priceCalculator.calculatePrice(TransportType.BIKE, 0);
         });
         Assertions.assertEquals("Distance should be more than 0 km", exception.getMessage());
     }
@@ -38,18 +88,9 @@ class PriceCalculatorTest {
     @DisplayName("Test CalculatorPrise bike overriding value")
     void calculatePriceForBikeIsOverridingValue() {
         IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            priceCalculator.calculatePrice(TransportType.BIKE, 25);
+            priceCalculator.calculatePrice(TransportType.BIKE, 21);
         });
         Assertions.assertEquals("Bike can not go for more than 20 km", exception.getMessage());
     }
-
-    @Test
-    @DisplayName("ew")
-    void calculatePriceForIncorrectTransport() {
-        UnsupportedOperationException exception = Assertions.assertThrows(UnsupportedOperationException.class,() -> {
-                    priceCalculator.calculatePrice(TransportType.DRONE, 10);
-                },"transport type is not handled correctly");
-    }
-
 
 }
